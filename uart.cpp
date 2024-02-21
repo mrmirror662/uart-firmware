89055430cdf3f22c96317e3a13591a82f437f13e
barath keshav <62153832+mrmirror662@users.noreply.github.com>
2024-02-21 16:46:17 +0530

    Update uart.cpp

--- uart.cpp
+++ uart.cpp
@@ -5,115 +5,135 @@
 #include <stdio.h>
 #include <avr/eeprom.h>
 
+// Define a buffer to store received bytes
 volatile static uint8_t rx_buffer[USB_EP_SIZE] = {0};
+
+// Track the number of received bytes
 volatile static uint16_t rx_count = 0;
+
+// Flag indicating UART transmitter status
 volatile static uint8_t uart_tx_busy = 1;
+
+// ISR for USART Receive Complete
 ISR(USART_RX_vect)
 {
-
+    // Track position to write into the receive buffer
     volatile static uint16_t rx_write_pos = 0;
 
+    // Read received byte and store in buffer
     rx_buffer[rx_write_pos] = UDR0;
+
+    // Increment received byte count
     rx_count++;
-    rx_write_pos++;
-    if (rx_write_pos >= USB_EP_SIZE)
-    {
+
+    // Move to next buffer position, wrap if necessary
+    if (++rx_write_pos >= USB_EP_SIZE)
         rx_write_pos = 0;
-    }
 }
 
+// ISR for USART Transmit Complete
 ISR(USART_TX_vect)
 {
+    // Set flag to indicate UART busy transmitting
     uart_tx_busy = 1;
 }
-
-void uart_init(uint32_t baud, uint8_t high_speed)
+class UART
 {
-
-    uint8_t speed = 16;
-
-    if (high_speed != 0)
+private:
+public:
+    // Initialize USART with given baud rate
+    void init(uint32_t baud = 2400)
     {
-        speed = 8;
-        UCSR0A |= 1 << U2X0;
+        // Default speed divisor
+        uint8_t speed = 16;
+
+        // Calculate baud rate register value
+        baud = (F_CPU / (speed * baud)) - 1;
+
+        // Set baud rate registers
+        UBRR0H = (baud & 0x0F00) >> 8;
+        UBRR0L = (baud & 0x00FF);
+
+        // Enable transmitter, receiver, and corresponding interrupts
+        UCSR0B |= (1 << TXEN0) | (1 << RXEN0) | (1 << TXCIE0) | (1 << RXCIE0);
     }
 
-    baud = (F_CPU / (speed * baud)) - 1;
-
-    UBRR0H = (baud & 0x0F00) >> 8;
-    UBRR0L = (baud & 0x00FF);
-
-    UCSR0B |= (1 << TXEN0) | (1 << RXEN0) | (1 << TXCIE0) | (1 << RXCIE0);
-}
-
-void uart_send_byte(uint8_t c)
-{
-    while (uart_tx_busy == 0)
-        ;
-    uart_tx_busy = 0;
-    UDR0 = c;
-}
-
-void uart_send_array(uint8_t *c, uint16_t len)
-{
-    for (uint16_t i = 0; i < len; i++)
+    void send(uint8_t c)
     {
-        uart_send_byte(c[i]);
+        // wait till not busy
+        while (uart_tx_busy == 0)
+            ;
+        uart_tx_busy = 0;
+        UDR0 = c;
     }
-}
-
-void uart_send_string(uint8_t *c)
-{
-    uint16_t i = 0;
-    do
+    void send(uint8_t *c, uint16_t len)
     {
-        uart_send_byte(c[i]);
-        i++;
-
-    } while (c[i] != '\0');
-    uart_send_byte(c[i]);
-}
-
-uint16_t uart_read_count(void)
-{
-    return rx_count;
-}
-
-uint8_t uart_read(void)
-{
-    static uint16_t rx_read_pos = 0;
-    uint8_t data = 0;
-
-    data = rx_buffer[rx_read_pos];
-    rx_read_pos++;
-    rx_count--;
-    if (rx_read_pos >= USB_EP_SIZE)
-    {
-        rx_read_pos = 0;
+        for (uint16_t i = 0; i < len; i++)
+            send(c[i]);
     }
-    return data;
-}
+
+    void send(uint8_t *c)
+    {
+        uint16_t i = 0;
+        do
+        {
+            send(c[i]);
+            i++;
+
+        } while (c[i] != '\0');
+        send(c[i]);
+    }
+    // gives number of bytes to be read
+    uint16_t read_count()
+    {
+        return rx_count;
+    }
+    // reads a byte
+    uint8_t read()
+    {
+        static uint16_t rx_read_pos = 0;
+        uint8_t data = 0;
+
+        data = rx_buffer[rx_read_pos];
+        rx_read_pos++;
+        rx_count--;
+        if (rx_read_pos >= USB_EP_SIZE)
+        {
+            rx_read_pos = 0;
+        }
+        return data;
+    }
+};
 #define MAX_EEPROM_SIZE 2048
+
 int main(void)
 {
+    // start and end messages
     const uint8_t start[] = "Program Start\n\r";
     const uint8_t end[] = "Program End\n\r";
     uint8_t data = 'A';
 
-    uart_init(2400, 0);
+    // create uart object and init
+    UART uart;
+    uart.init();
+
+    // enable interrupts
     sei();
-    uart_send_string(start);
+
+    uart.send((uint8_t *)start);
+    // initialize start address for EEPROM
     uint8_t *s = 64;
     uint8_t *index = s;
     uint16_t counter = 0;
     while (1)
     {
+        // if overflow exit
         if (counter > MAX_EEPROM_SIZE)
             break;
-        if (uart_read_count() > 0)
+        if (uart.read_count() > 0)
         {
 
-            data = uart_read();
+            data = uart.read();
             if (data == '\r')
                 break;
             eeprom_write_byte(index, data);
@@ -130,6 +150,6 @@ int main(void)
     {
         c = eeprom_read_byte(index);
         index++;
-        uart_send_byte(c);
+        uart.send(c);
     }
 }
